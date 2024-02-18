5ab39894d621017f9b2d61714bd7702143d1b02c
MasatoIkeda05 <2112136e@stu.kobe-u.ac.jp>
2024-02-19 03:17:41 +0900

    [add] ユーザー登録時のバリデーションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'timesheets/home'
   get 'signup' => 'users#new'
   get 'index' => 'timesheets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
