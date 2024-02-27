109ec837a4fecf99cc2c759e10d217ab3e6dc538
MasatoIkeda05 <2112136e@stu.kobe-u.ac.jp>
2024-02-27 09:10:41 +0900

    [fix] カラム名の変更

--- db/migrate/20240226051533_rename_columns_in_workingtimes.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumnsInWorkingtimes < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :workingtimes, :start_time, :check_in
-    rename_column :workingtimes, :finish_time, :check_out
-    rename_column :workingtimes, :working_time, :work_min
-  end
-end
--- db/migrate/20240226052328_rename_columns_in_workingtimes_v2.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnsInWorkingtimesV2 < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :workingtimes, :work_min, :working_min
-  end
-end
